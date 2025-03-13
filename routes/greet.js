0dbbd424bdb694693dbfd5c2068253473e641d92
Darien Bernal <darien.bernal@gmail.com>
2025-03-13 14:20:17 -0400

    Created image.ejs

--- routes/greet.js
+++ routes/greet.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', (req, res) => {
-    const {name} = req.params
-    res.render('greet', {title: "Greet", name})
+    const { name } = req.params
+    res.render('greet', { title: "Greet", name })
 })
 
 module.exports = router
