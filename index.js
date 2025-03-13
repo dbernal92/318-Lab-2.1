cdb3309bfdb09c271c608958f42e67f6a6ccafd0
Darien Bernal <darien.bernal@gmail.com>
2025-03-13 15:08:58 -0400

    Completed img download functionality

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const morgan = require('morgan')
 
 const aboutRoute = require('./routes/about')
 const greetRoute = require('./routes/greet')
+const imageRoute = require('./routes/image')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
-// Setting view engine as ejs
+//setting the view engine as ejs
 app.set('view engine', 'ejs')
+
+app.use(express.static('images'))
 
 app.use('/about', aboutRoute)
 app.use('/greet', greetRoute)
+app.use('/image', imageRoute)
 
 app.get('/', (req, res) =>{
-    res.send("This is our home page")
+    res.render('index', {title: "Home"})
+})
+
+app.post('/submit-form', (req, res)=> {
+    console.log(req.body);
+    res.send("Form submitted successfully")
 })
 
 app.listen(PORT, () => {
