cbf84763d9d6189229b78cee9f00e83ee2a75c67
Darien Bernal <darien.bernal@gmail.com>
2025-03-13 13:41:13 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const PORT = 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const aboutRoute = require('./routes/about')
+const greetRoute = require('./routes/greet')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+// Setting view engine as ejs
+app.set('view engine', 'ejs')
+
+app.use('/about', aboutRoute)
+app.use('/greet', greetRoute)
+
+app.get('/', (req, res) =>{
+    res.send("This is our home page")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
